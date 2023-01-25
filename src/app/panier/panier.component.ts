a5fab85c00962b62a569ffdac3ae278209a84385
l3miage-moussola <adam.moussolna@etu.univ-grenoble-alpes.fr>
2023-01-26 00:48:14 +0100

    Feat : panier subscribed au service

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -37,7 +37,7 @@ export class PanierComponent{
     { CIP7: 100922, libelle: 'Doliprane',quantity: 2, price: 45.99 },
     { CIP7: 100992, libelle: 'Tramadol',quantity: 6, price: 29.99 },
     { CIP7: 103622, libelle: 'Boite de jsp quoi', quantity: 1, price: 19.99 }
-  ];*/
+  ]; */
   get totalPrice() {
 
     return this.contenuPanier.reduce((total:number, item:any)=>total+item.price)
