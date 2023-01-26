a1a9232946b06e2143e75333095bd5db1279cdbf
l3miage-moussola <adam.moussolna@etu.univ-grenoble-alpes.fr>
2023-01-26 11:02:13 +0100

    Feat : homeService getListPresentaion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent {
 
   constructor(private homeService: HomeService) {
 
-    //this.listPresentation=this.homeService.getListPresentationTot();
+     //this.listPresentation=this.homeService.getListPresentationTot();
     this.homeService.getListPresentationTot().subscribe(res=>this.listPresentation=res);
     console.log("exemple"+this.listPresentation.data);
 
