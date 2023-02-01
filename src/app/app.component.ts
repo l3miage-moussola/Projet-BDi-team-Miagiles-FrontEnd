9089983aaa8233f303cdd88047eb36aadc412812
Camille <camille.cabrol1@etu.univ-grenoble-alpes.fr>
2023-02-01 09:42:40 +0100

    Remove admin and not admin users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,6 @@ import { NgOptimizedImage } from '@angular/common'
 export class AppComponent implements OnInit {
   private roles: string[] = [];
   isLoggedIn = false;
-  showAdminBoard = false;
-  showModeratorBoard = false;
   username?: string;
 
 
@@ -23,9 +21,6 @@ export class AppComponent implements OnInit {
     if (this.isLoggedIn) {
       const user = this.tokenStorageService.getUser();
       this.roles = user.roles;
-
-      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
-      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
 
       this.username = user.username;
     }
